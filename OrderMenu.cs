f7123a8bb055d380320e42086d274f906c549195
grantedens <96454545+grantedens@users.noreply.github.com>
2022-11-23 18:02:33 -0600

    Create OrderMenu.cs

---
+++ OrderMenu.cs
@@ -0,0 +1,119 @@
+namespace Project2fr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double fruitSaladPrice = 9.95;
+        double pastaSaladPrice = 12;
+        double smoothiePrice = 4.95;
+        double fruitJuicePrice = 3.95;
+        double cupcakePrice = 3;
+        double shortcakePrice = 6;
+        double saladPrice, drinkPrice, dessertPrice, saladCost, drinkCost, dessertCost, totalCost;
+        double fruitSaladQuantity, pastaSaladQuantity, smoothieQuantity, fruitJuiceQuantity, cupcakeQuantity, shortcakeQuantity, totalQuantity, drinkQuantity, dessertQuantity, saladQuantity;
+
+        private void clearButt_Click_1(object sender, EventArgs e)
+        {
+            saladLbl.Text = "Salads Label";
+            dessertLbl.Text = "Dessert Label";
+            drinksLbl.Text = "Drinks Label";
+            saladPriceTb.Text = "0";
+            saladQuantityTb.Text = "0";
+            dessertPriceTb.Text = "0";
+            dessertQuantityTb.Text = "0";
+            drinkPriceTb.Text = "0";
+            drinkQuantityTb.Text = "0";
+            cupcakeRdo.Checked = false;
+            shortcakeRdo.Checked = false;
+            fruitJuiceRdo.Checked = false;
+            pastaSaladRdo.Checked = false;
+            fruitSaladRdo.Checked = false;
+            smoothieRDB.Checked = false;
+        }
+
+        private void calcButt_Click(object sender, EventArgs e)
+        {
+            saladQuantity = Convert.ToDouble(saladQuantityTb.Text);
+            saladCost =  saladQuantity * saladPrice;
+            dessertQuantity = Convert.ToDouble(dessertQuantityTb.Text);
+            dessertCost = dessertQuantity * dessertPrice;
+            drinkQuantity = Convert.ToDouble(drinkQuantityTb.Text);
+            drinkCost = drinkQuantity * drinkPrice;
+            totalCost = dessertCost + drinkCost + saladCost;
+            totalQuantity = dessertQuantity + drinkQuantity + saladQuantity;
+            richTextBox1.Text = "The total sales was $" + totalCost.ToString() + "\n The total items sold were " + totalQuantity.ToString();
+
+            //richTextBox1.Text = saladCost.ToString();
+        }
+
+        private void fruitJuiceRdo_CheckedChanged(object sender, EventArgs e)
+        {
+            drinkPrice = 3.95;
+            drinkPriceTb.Text = "$3.95";
+            drinksLbl.Text = "Drinks Selection: Fruit Juice";
+
+        }
+        private void cupcakeRdo_CheckedChanged(object sender, EventArgs e)
+        {
+            dessertPrice = 3;
+            dessertPriceTb.Text = "$3.00";
+            dessertLbl.Text = "Dessert Choice: Cupcake";
+        }
+
+        private void shortcakeRdo_CheckedChanged(object sender, EventArgs e)
+        {
+            dessertPrice = 6;
+            dessertPriceTb.Text = "$6.00";
+            dessertLbl.Text = "Dessert Choice: Shortcake";
+        }
+
+       
+
+        private void smoothieRDB_CheckedChanged(object sender, EventArgs e)
+        {
+            drinkPrice = 4.95;
+            drinkPriceTb.Text = "$4.95";
+            drinksLbl.Text = "Drinks Selection: Smoothie";
+
+        }
+
+        private void pastaSaladRdo_CheckedChanged(object sender, EventArgs e)
+        {
+            saladPrice = 12;
+            saladPriceTb.Text = "$12.00";
+            saladLbl.Text = "Salads Selection: Pasta Salad";
+
+        }
+
+        private void fruitSaladRdo_CheckedChanged(object sender, EventArgs e)
+        {
+            saladPrice = 9.95;
+            saladPriceTb.Text = "$9.95";
+            saladLbl.Text = "Salads Selection: Fruit Salad";
+        }
+
+        private void exitButt_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
