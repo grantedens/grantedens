ef53e9017addd34fd76f6561c7cd76efcb986ccc
grantedens <96454545+grantedens@users.noreply.github.com>
2023-02-28 15:35:42 -0600

    Create NFTcreation.sol

---
+++ NFTcreation.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.8.1/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.8.1/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.8.1/access/Ownable.sol";
+
+/// @custom:security-contact Grant.edens2000@gmail.com
+contract EdensNFT is ERC721, ERC721URIStorage, Ownable {
+    constructor() ERC721("EdensNFT", "ENFT") {}
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
